da8ca58676db354b3e8a2aa5df19e23c916c0a88
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-04-05 13:00:16 -0600

    Removed math demonstration from scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,4 @@ let myName = "Evan";
 document.write(myName);
 /*myName = 'John Doe';
 document.write(myName);*/
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
 alert('Hello world!')
