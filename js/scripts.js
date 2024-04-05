89959e5a11e657f51cc7a7aed646ab1b11733993
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-04-05 12:55:30 -0600

    Initial files created for 'simple-js-app'

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Stylelint keeps telling me whatever the first thing in the Line is an error.
+let myName = "Evan";
+document.write(myName);
+/*myName = 'John Doe';
+document.write(myName);*/
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+alert('Hello world!')
