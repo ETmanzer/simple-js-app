8099dad948875bed93a8e0b2ba48b7a97a6bb3c6
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-04-08 11:11:14 -0600

    changed double quotes to single quotes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // height in m
 let pokemonList = [
-    {name: "Bulbasaur", type: ["grass","poison"], height: 0.7},
-    {name: "Ivysaur", type: ["grass","poison"], height: 1},
-    {name: "Venosaur", type: ["grass","poison"], height: 2}
+    {name: 'Bulbasaur', type: ['grass','poison'], height: 0.7},
+    {name: 'Ivysaur', type: ['grass','poison'], height: 1},
+    {name: 'Venosaur', type: ['grass','poison'], height: 2}
 ];
 // Data gathered from https://pokedex.org/
