257b2f42b1c03480af2b1948f33265945c3da8d7
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-04-10 09:13:12 -0600

    added note to figure out how to line break and space the pokemonList info

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,6 @@ let pokemonList = [
 ];
 
 for (let i=0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');// have to figure out how to line break
 }
 // Data gathered from https://pokedex.org/
