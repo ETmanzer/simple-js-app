853aaeb05b13dd68fcbed2d2f797476dafab56bc
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-04-10 09:02:03 -0600

    First try at for loop to display Pokemon name and height

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,8 @@ let pokemonList = [
     {name: 'Ivysaur', type: ['grass','poison'], height: 1},
     {name: 'Venosaur', type: ['grass','poison'], height: 2}
 ];
+
+for (let i=0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+}
 // Data gathered from https://pokedex.org/
