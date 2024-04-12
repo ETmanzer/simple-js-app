aea898ad8bc61639728265629145d42c1cfe9c82
ETmanzer <163933890+ETmanzer@users.noreply.github.com>
2024-04-12 11:34:36 -0600

    Wrapped pokemonList in IIFE and created new vairable pokemonRepositry to hold IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 // height in m
-let pokemonList = [
-    {name: 'Bulbasaur', type: ['grass','poison'], height: 0.7},
-    {name: 'Ivysaur', type: ['grass','poison'], height: 1},
-    {name: 'Venosaur', type: ['grass','poison'], height: 2}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', type: ['grass','poison'], height: 0.7},
+        {name: 'Ivysaur', type: ['grass','poison'], height: 1},
+        {name: 'Venosaur', type: ['grass','poison'], height: 2}
+    ];
 
-pokemonList.forEach(function(pokemon) {
-    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')</p>');
-});
+    pokemonList.forEach(function(pokemon) {
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')</p>');
+    });
+})();
 // Data gathered from https://pokedex.org/
